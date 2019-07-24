be82a401f3d37b8f9ca8deb4874e8b1b3975c9dc
Dave.S <52350248+davestw@users.noreply.github.com>
2019-07-24 22:20:37 +0700

    Add files via upload
    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Exercise4_Sattawat_M.py
@@ -0,0 +1,7 @@
+a = 75.5,83.8,80,95.8
+print("---- You Score ----")
+print("Foundation English :",a[0],"คะแนน")
+print("General Business :",a[1],"คะแนน")
+print("Introduction to Computer Systems :",a[2],"คะแนน")
+print("Computer Programming :",a[3],"คะแนน")
+
